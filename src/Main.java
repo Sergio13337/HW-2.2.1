a747b856e67bd1314f489c663e68c97a1ee55a43
pc <mark1337@internet.ru>
2022-08-25 14:37:53 +0200

    Выполненное ДЗ по переменным 2.2.1.1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,66 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //Задание 1
+        var dog = 8;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        //Задание 2
+        var dog2 = dog + 4;
+        var cat2 = cat + 4;
+        var paper2 = paper + 4;
+        System.out.println(dog2);
+        System.out.println(cat2);
+        System.out.println(paper2);
+
+        //Задание 3
+        var dog3 = dog2 - 3.5;
+        var cat3 = cat2 - 1.6;
+        var paper3 = paper2 - 7639;
+        System.out.println(dog3);
+        System.out.println(cat3);
+        System.out.println(paper3);
+
+        //Задание 4
+        var friend = 19;
+        System.out.println(friend);
+        var friend1 = friend + 2;
+        System.out.println(friend1);
+        var friend2 = friend1 / 7;
+        System.out.println(friend2);
+
+        //Задание 5
+        var frog = 3.5;
+        System.out.println(frog);
+        var frog1 = frog * 10;
+        System.out.println(frog1);
+        var frog2 = frog1 / 3.5;
+        System.out.println(frog2);
+        var frog3 = frog2 + 4;
+        System.out.println(frog3);
+
+        //Задание 6,7
+        var weightBox1 = 78.2;
+        var weightBox2 = 82.7;
+        var generalWeight = weightBox1 + weightBox2;
+        System.out.println(generalWeight);
+        var difference = weightBox2 - weightBox1;
+        System.out.println(difference);
+        var difference2 = weightBox2 % weightBox1;
+        System.out.println(difference2);
+
+        //Задание 8
+        var people = 640 / 8;
+        System.out.println("Всего работников в компании - " + people + " человек");
+         var sumPeople = people + 94;
+         var time = sumPeople * 8;
+        System.out.println("Если в компании работает " + sumPeople + " человек, то всего " + time + " часов работы может быть поделено между сотрудниками.");
+
+
 
 
 
